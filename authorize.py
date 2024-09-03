466381194e5c6fe541ad913b72d41b5de2fc8bca
jacobnarvades <116325929+jacobnarvades@users.noreply.github.com>
2024-09-02 21:46:29 -0400

    authorize.py

--- authorize.py
+++ authorize.py
@@ -3,8 +3,8 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 def auth_credentials():
     # Set up a session
-    client_id = "f09683829a904df38515406f320e0fc1"
-    client_secret = "e808ee01467440f6ad0b4ef4ade9c7bf"
+    client_id = "CLIENT_ID"
+    client_secret = "CLIENT_SECRET"
     
     auth_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
     
